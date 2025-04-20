e75ec93865367c3877ef65944a8b3fee3e3fcf42
unknown <2020583117@qq.com>
2025-04-20 14:06:59 +0800

    feat: 添加prisma链接数据库并尝试部署

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import { serve } from "@hono/node-server";
+import prisma from "../lib/prisma";
 
 const app = new Hono().basePath("/api");
 
@@ -8,10 +9,20 @@ app.get("/", (c) => {
 	return c.text("Hello Hono!");
 });
 
+// 获取所有已发布的帖子
+app.get("/user", async (c) => {
+	try {
+		const posts = await prisma.user.findMany();
+		return c.json(posts);
+	} catch (error) {
+		console.error("Failed to fetch posts:", error);
+		return c.json({ error: "Failed to fetch posts" }, 500);
+	}
+});
+
 // 添加本地开发服务器启动逻辑
-const port = 5000; // 或者你想要的其他端口
+const port = 5000;
 if (process.env.NODE_ENV !== "production") {
-	// 确保只在本地开发时运行
 	console.log(`Server is running on port ${port}`);
 	serve({
 		fetch: app.fetch,
@@ -25,4 +36,5 @@ export const GET = handler;
 export const POST = handler;
 export const PATCH = handler;
 export const PUT = handler;
+export const DELETE = handler;
 export const OPTIONS = handler;
